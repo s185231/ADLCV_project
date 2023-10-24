aa542a7f2a4216fbedd4b0171685ef8a12f0ac02
luchsonice <s194369@student.dtu.dk>
2023-10-24 13:40:45 +0200

    model

--- src/model.py
+++ src/model.py
@@ -2,144 +2,169 @@ from typing import Dict, List, Optional
 import pytorch_lightning as pl
 import torch
 from torch import nn
+from torch.nn import functional as F
 
-
-class Block(nn.Module):
-    def __init__(self, n_features):
-        super(Block, self).__init__()
-        self.Fx = nn.Sequential(
-            nn.Conv2d(n_features, n_features, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(n_features, n_features, kernel_size=3, padding=1),
+class SelfAttention(pl.LightningModule):
+    def __init__(self, channels, size):
+        super(SelfAttention, self).__init__()
+        self.channels = channels
+        self.size = size
+        self.mha = nn.MultiheadAttention(channels, 4, batch_first=True)
+        self.ln = nn.LayerNorm([channels])
+        self.ff_self = nn.Sequential(
+            nn.LayerNorm([channels]),
+            nn.Linear(channels, channels),
+            nn.GELU(),
+            nn.Linear(channels, channels),
         )
 
     def forward(self, x):
-        fx = self.Fx(x)
-        x = nn.ReLU()(fx + x)
-        return x
+        x = x.view(-1, self.channels, self.size * self.size).swapaxes(1, 2)
+        x_ln = self.ln(x)
+        attention_value, _ = self.mha(x_ln, x_ln, x_ln)
+        attention_value = attention_value + x
+        attention_value = self.ff_self(attention_value) + attention_value
+        return attention_value.swapaxes(2, 1).view(-1, self.channels, self.size, self.size)
 
 
-class BatchBlock(nn.Module):
-    def __init__(self, n_features):
-        super(BatchBlock, self).__init__()
-        self.Fx = nn.Sequential(
-            nn.Conv2d(n_features, n_features, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(n_features),
-            nn.Conv2d(n_features, n_features, kernel_size=3, padding=1),
+class DoubleConv(pl.LightningModule):
+    def __init__(self, in_channels, out_channels, mid_channels=None, residual=False):
+        super().__init__()
+        self.residual = residual
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.GroupNorm(1, mid_channels),
+            nn.GELU(),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.GroupNorm(1, out_channels),
         )
 
-        self.bat = nn.BatchNorm2d(n_features)
-
     def forward(self, x):
-        fx = self.Fx(x)
-        x = nn.ReLU()(fx + x)
-        x = self.bat(x)
-        return x
+        if self.residual:
+            return F.gelu(x + self.double_conv(x))
+        else:
+            return self.double_conv(x)
+
+
+class Down(pl.LightningModule):
+    def __init__(self, in_channels, out_channels, emb_dim=256):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, in_channels, residual=True),
+            DoubleConv(in_channels, out_channels),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_dim,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, t):
+        x = self.maxpool_conv(x)
+        emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+
+
+class Up(pl.LightningModule):
+    def __init__(self, in_channels, out_channels, emb_dim=256):
+        super().__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.conv = nn.Sequential(
+            DoubleConv(in_channels, in_channels, residual=True),
+            DoubleConv(in_channels, out_channels, in_channels // 2),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(
+                emb_dim,
+                out_channels
+            ),
+        )
+
+    def forward(self, x, skip_x, t):
+        x = self.up(x)
+        x = torch.cat([skip_x, x], dim=1)
+        x = self.conv(x)
+        emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
 
 
 class Model(pl.LightningModule):
     def __init__(
         self,
-        num_blocks: int = 2,
-        num_features: int = 64,
-        lr: Optional[float] = 1e-3,
-        weight_decay: Optional[float] = 0,
-        batch_size: Optional[int] = 1,
-        batch_normalization: Optional[bool] = False,
-        optimizer: Optional[str] = None,
+        img_size: int = 16,
+        c_in: int = 3,
+        c_out: int = 3,
+        time_dim: int = 256,
+        device: str = "cpu",
+        channels: int = 32,
         *args,
         **kwargs
-    ) -> None:
+        ) -> None:
         super(Model, self).__init__(*args, **kwargs)
 
-        conv_layers = [
-            nn.Conv2d(3, num_features, kernel_size=7, stride=2, padding=3),
-            nn.ReLU(),
-        ]
-        if batch_normalization:
-            for i in range(num_blocks):
-                conv_layers.append(BatchBlock(num_features))
-        else:
-            for i in range(num_blocks):
-                conv_layers.append(Block(num_features))
 
-        self.blocks = nn.Sequential(*conv_layers)
+        self.device = device
+        self.time_dim = time_dim
+        self.inc = DoubleConv(c_in, channels)
+        self.down1 = Down(channels, channels*2,  emb_dim=time_dim)
+        self.sa1 = SelfAttention(channels*2, img_size//2)
+        self.down2 = Down(channels*2, channels*4, emb_dim=time_dim)
+        
+        self.sa2 = SelfAttention(channels*4, img_size // 4)
+        self.down3 = Down(channels*4, channels*4,  emb_dim=time_dim)
+        self.sa3 = SelfAttention(channels*4, img_size // 8)
 
-        self.rd = nn.MaxPool2d(2)
+        self.bot1 = DoubleConv(channels*4, channels*8)
+        self.bot2 = DoubleConv(channels*8, channels*8)
+        self.bot3 = DoubleConv(channels*8, channels*4)
 
-        self.fc = nn.Sequential(
-            nn.Linear(32 * 32 * num_features, 512),
-            nn.ReLU(),
-            nn.Linear(512, 1),
-            nn.Sigmoid(),
+        self.up1 = Up(channels*8, channels*2,  emb_dim=time_dim)
+        self.sa4 = SelfAttention(channels*2, img_size // 4)
+        self.up2 = Up(channels*4, channels,  emb_dim=time_dim)
+        self.sa5 = SelfAttention(channels, img_size // 2)
+        self.up3 = Up(channels*2, channels,  emb_dim=time_dim)
+        self.sa6 = SelfAttention(channels, img_size)
+        self.outc = nn.Conv2d(channels, c_out, kernel_size=1)
+
+    def pos_encoding(self, t, channels):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, channels, 2, device=self.device).float() / channels)
         )
-        self.lr = lr
-        self.batch_size = batch_size
-        self.loss = torch.nn.BCELoss()
-        if optimizer is None or optimizer == "Adam":
-            self.optimizer = torch.optim.Adam(
-                self.parameters(), lr=self.lr, weight_decay=weight_decay
-            )
-        if optimizer == "SGD":
-            self.optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
+        pos_enc_a = torch.sin(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, channels // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc
 
-    def forward(self, x: List[str]) -> List[str]:
-        """
-        https://huggingface.co/docs/transformers/model_doc/t5#inference
-        """
+    def forward(self, x, t):
+        
+        t = t.unsqueeze(-1).type(torch.float)
+        t = self.pos_encoding(t, self.time_dim)
+        x1 = self.inc(x)
+        x2 = self.down1(x1, t)
+        x2 = self.sa1(x2)
+        x3 = self.down2(x2, t)
+        x3 = self.sa2(x3)
+        x4 = self.down3(x3, t)
+        x4 = self.sa3(x4)
+        x4 = self.bot1(x4)
+        x4 = self.bot2(x4)
+        x4 = self.bot3(x4)
+        x = self.up1(x4, x3, t)
+        x = self.sa4(x)
+        x = self.up2(x, x2, t)
+        x = self.sa5(x)
+        x = self.up3(x, x1, t)
+        x = self.sa6(x)
+        output = self.outc(x)
 
-        x = self.blocks(x)
-
-        x = self.rd(x)
-        # reshape x so it becomes flat, except for the first dimension (which is the minibatch)
-        x = x.view(x.size(0), -1)
-        out = self.fc(x)
-        return out
-
-    def _inference_training(
-        self, batch, batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        """
-        From https://huggingface.co/docs/transformers/model_doc/t5#training
-        """
-
-        data, target = batch
-        output = self(data)
-        output, target = output.flatten(), target.flatten()
-        preds = torch.round(output)
-        accuracy = self.accuracy(preds, target)
-        target = target.to(torch.float32)
-
-        return self.loss(output, target), accuracy
-
-    def training_step(
-        self, batch: List[str], batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        loss, accuracy = self._inference_training(batch, batch_idx)
-        self.log("train loss", loss, batch_size=self.batch_size)
-        self.log("train accuracy", accuracy, batch_size=self.batch_size)
-        return loss
-
-    def validation_step(
-        self, batch: List[str], batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        loss, accuracy = self._inference_training(batch, batch_idx)
-        self.log("val loss", loss, batch_size=self.batch_size, sync_dist=True)
-        self.log("val accuracy", accuracy, batch_size=self.batch_size, sync_dist=True)
-        return loss
-
-    def test_step(
-        self, batch: List[str], batch_idx: Optional[int] = None
-    ) -> torch.Tensor:
-        loss, accuracy = self._inference_training(batch, batch_idx)
-        self.log("test loss", loss, batch_size=self.batch_size, sync_dist=True)
-        self.log("test accuracy", accuracy, batch_size=self.batch_size, sync_dist=True)
-
-        return loss
-
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        return self.optimizer
-
-    def accuracy(self, preds, target):
-        return (preds == target).sum() / len(target)
+        return output
