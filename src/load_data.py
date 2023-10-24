1db1b95c6c7f084ec0985cd292c1efe320b0afeb
s185231 <s185231@hypatia.fysik.dtu.dk>
2023-10-24 11:39:01 +0200

    dataloader

--- src/load_data.py
+++ src/load_data.py
@@ -1,47 +1,63 @@
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
-from torch.utils.data import random_split
+from torch.utils.data import random_split, Dataset
+from PIL import Image
 import os
 from src import _DATA_PATH
 
+class ExposureDataset(Dataset):
+    def __init__(self,state,ev, transform):
 
-def get_dataloaders(batch_size, num_workers=8):
+        self.transform = transform
+        self.files = os.listdir(os.path.join(_DATA_PATH, state, "INPUT_IMAGES"))
+
+        self.ev_list = []
+        self.target_list = []
+        for file in self.train_files:
+            if file.split('_')[-1][0] == ev:
+                self.ev_list.append(file)
+            elif file.split('_')[-1][0] == '0':
+                self.target_list.append(file)
+        
+        def __len__(self):
+            return len(self.ev_list)
+        
+        def __getitem__(self, idx):
+            img = Image.open(self.ev_list[idx])
+            img = self.transform(img)
+            target = Image.open(self.target_list[idx])
+            target = self.transform(target)
+            return img, target
+
+def get_dataloaders(dataset, ev, batch_size, num_workers=8):
+    assert (ev == 'P' or ev == 'N'), "Please input P or N"
+
     data_transform = transforms.Compose(
         [
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            #transforms.RandomRotation(90),
-            #transforms.RandomVerticalFlip(p=0.5),
+            transforms.RandomRotation(90),
+            transforms.RandomVerticalFlip(p=0.5),
             #transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
             #transforms.GaussianBlur(kernel_size=5),
-            transforms.Resize((128, 128), antialias=None)
+            transforms.Resize((512, 512), antialias=None)
         ]
     )
     data_transform_test = transforms.Compose(
         [
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            transforms.Resize((128, 128), antialias=None)
+            transforms.Resize((512, 512), antialias=None)
         ]
     )
-    # load train data from "/u/data/s194333/DLCV/project1_02514/data/train/hotdog"
-    trainset = datasets.ImageFolder(
-        root=os.path.join(_DATA_PATH, "train"), transform=data_transform
-    )
-    testset = datasets.ImageFolder(
-        root=os.path.join(_DATA_PATH, "test"), transform=data_transform
-    )
-    testset2 = datasets.ImageFolder(
-        root=os.path.join(_DATA_PATH, "test"), transform=data_transform_test
-    )
-    generator1 = torch.Generator().manual_seed(42)
-    valset_new, testset_new = random_split(testset, [0.5, 0.5], generator=generator1)
-    valset_new2, testset_new2 = random_split(testset, [0.5, 0.5], generator=generator1)
-    trainloader = DataLoader(
-        trainset, batch_size=batch_size, num_workers=num_workers, shuffle=False#True
-    )
-    valloader = DataLoader(valset_new, batch_size=batch_size, num_workers=num_workers)
-    testloader = DataLoader(testset_new2, batch_size=batch_size, num_workers=num_workers)
+
+    trainset = ExposureDataset("training", ev, transform=data_transform)
+    valset = ExposureDataset("validation", ev, transform=data_transform_test)
+    testset = ExposureDataset("testing", ev, transform=data_transform_test)
+
+    trainloader = DataLoader(trainset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
+    valloader = DataLoader(valset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+    testloader = DataLoader(testset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
 
     return trainloader, valloader, testloader
